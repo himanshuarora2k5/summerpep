e3668a2e36a77b02bd286562e6bd3a2ad22fa969
Himanshu Arora <himanshuarora2681@gmail.com>
2026-06-26 09:43:38 +0530

    day 4 content

--- 25jun/strfrq.cpp
+++ 25jun/strfrq.cpp
@@ -1,4 +1,4 @@
-//take string input and output each char's frequency by making a 26 character 2d array with each letter being assigned a number and working as a key and updating the value of each key as per the string, abbcdde --> a1b2c1d2e1
+//take string input and output each char's frequency by making a 26 character array with index as a key and updating the value of each key as per the string, abbcdde --> a1b2c1d2e1
 #include <iostream>
 #include <string>
 using namespace std;
@@ -6,20 +6,15 @@ int main() {
     string s;
     cin >> s;
 
-    int freq[26][2];
-
-    for(int i = 0; i < 26; i++) {
-        freq[i][0] = 'a' + i; // store character
-        freq[i][1] = 0;       // frequency
-    }
+    int freq[26]={0};
 
     for(char ch : s) {
-        freq[ch - 'a'][1]++;
+        freq[ch - 'a']++;
     }
 
     for(int i = 0; i < 26; i++) {
-        if(freq[i][1] > 0) {
-            cout << char(freq[i][0]) << freq[i][1];
+        if(freq[i] > 0) {
+            cout << char('a' + i) << freq[i];
         }
     }
 
